0cd5f14f4dbe545c83042b192e951fde54cb5658
Denis Zajchenko <denis.zaichenko@gmail.com>
2016-06-07 07:43:33 +0300

    - fix not display default variables - fix the end point in ng2-http-get

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ fs.writeFileSync(`${__dirname}/../dist/ng2-templates.xml`, webshtormXML, 'utf8')
 console.log('\x1b[32m', "- created", '\x1b[0m', "/dist/ng2-templates.xml");
 
 function convert(vsTemplate, type) {
-  const regex = /\$\{([^\}]+)\}/g;
+  const regex = /\$\{([^\}|^0]+)\}/g;
   let webstormXml = '';
   let context =
       `<context>
@@ -51,12 +51,13 @@ function convert(vsTemplate, type) {
         });
       }
       line = line.replace('$0', '$END$');
+      line = line.replace('${0}', '$END$');
       line = escape(line);
       body.push(line);
     }
     if (vars.length) {
       for (const name of vars) {
-        varsXml.push(`<variable name="${name}" defaultValue="${name}" alwaysStopAt="true"/>`);
+        varsXml.push(`<variable name="${name}" defaultValue="&quot;${name}&quot;" alwaysStopAt="true"/>`);
       }
     }
 
